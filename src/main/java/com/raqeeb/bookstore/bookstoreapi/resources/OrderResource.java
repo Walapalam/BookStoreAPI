d54c322cd3f43e922869966dc2bdb4b66a131747
Raqeeb Rameez <103761347+Walapalam@users.noreply.github.com>
2025-04-23 16:07:29 +0000

    Contains some fixes for the mvn clean install, removed an irrelevant endpoint.

--- src/main/java/com/raqeeb/bookstore/bookstoreapi/resources/OrderResource.java
+++ src/main/java/com/raqeeb/bookstore/bookstoreapi/resources/OrderResource.java
@@ -7,6 +7,7 @@ package com.raqeeb.bookstore.bookstoreapi.resources;
 import com.raqeeb.bookstore.bookstoreapi.service.OrderService;
 import com.raqeeb.bookstore.bookstoreapi.model.Order;
 import com.raqeeb.bookstore.bookstoreapi.exception.InvalidInputException;
+import com.raqeeb.bookstore.bookstoreapi.exception.OrderNotFoundException;
 import com.raqeeb.bookstore.bookstoreapi.exception.BookNotFoundException;
 
 import javax.ws.rs.*;
@@ -71,21 +72,6 @@ public class OrderResource {
         }
     }
     
-    @PUT
-    @Path("/{orderId}/status")
-    public Response updateOrderStatus(
-            @PathParam("orderId") String orderId,
-            @QueryParam("status") String status) {
-        try {
-            Order updatedOrder = orderService.updateOrderStatus(orderId, status);
-            return Response.ok(updatedOrder).build();
-        } catch (InvalidInputException e) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                         .entity(e.getMessage())
-                         .build();
-        }
-    }
-    
     @DELETE
     @Path("/{orderId}")
     public Response cancelOrder(@PathParam("orderId") String orderId) {
@@ -94,6 +80,14 @@ public class OrderResource {
             return Response.noContent().build();
         } catch (InvalidInputException e) {
             return Response.status(Response.Status.BAD_REQUEST)
+                         .entity(e.getMessage())
+                         .build();
+        } catch (BookNotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND)
+                         .entity(e.getMessage())
+                         .build();
+        } catch (OrderNotFoundException e){
+            return Response.status(Response.Status.NOT_FOUND)
                          .entity(e.getMessage())
                          .build();
         }
